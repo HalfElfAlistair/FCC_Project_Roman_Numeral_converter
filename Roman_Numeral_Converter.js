b88309636526a585021c2967d200787b842eb56d
HalfElfAlistair <ajss90@hotmail.co.uk>
2021-02-08 18:43:43 +0000

    Refactored JavaScript code for a shorter, simpler function

--- Roman_Numeral_Converter.js
+++ Roman_Numeral_Converter.js
@@ -1,106 +1,23 @@
 function decimalToNumeral() {
-        let decimalInputValue = Number(document.getElementById("decimal-input").value);
-        let numeralArray = [];
-        let oneFigureArray = [];
-        let twoFigureArray = [];
-        let threeFigureArray = [];
-        let fourFigureArray = [];
-        let num = decimalInputValue;
-        let numberToConvert = num.toString().split("");
     
-        // Assigns an index to a relative array based on multiples of 10.
-    
-        if (num < 10) {
-            oneFigureArray.push(numberToConvert);
-        } else if ((num >= 10) && (num < 100)) {
-            twoFigureArray.push(numberToConvert[0]);
-            oneFigureArray.push(numberToConvert[1]);
-        } else if ((num >= 100) && (num < 1000)) {
-            threeFigureArray.push(numberToConvert[0]);
-            twoFigureArray.push(numberToConvert[1]);
-            oneFigureArray.push(numberToConvert[2]);
-        } else if ((num >= 1000) && (num < 4000)) {
-            fourFigureArray.push(numberToConvert[0]);
-            threeFigureArray.push(numberToConvert[1]);
-            twoFigureArray.push(numberToConvert[2]);
-            oneFigureArray.push(numberToConvert[3]);
-        } else if (num >= 4000) {
-            document.getElementById("numeral").innerHTML = 'undefined';
-        }
-    
-    
-        // Assigns a numeral for 1000 and multiples less than 4000.
-    
-        if (fourFigureArray <= 3) {
-            for (let i = 0; i < fourFigureArray; i++) {
-             numeralArray.unshift("M");
-             }
-        }
-    
-        // Assigns numerals for values between 100 and 1000.
-    
-        if (threeFigureArray <= 3) {
-            for (let i = 0; i < threeFigureArray; i++) {
-             numeralArray.push("C");
-             }
-         } else if (threeFigureArray == 4) {
-             numeralArray.push("CD");
-         } else if (threeFigureArray == 5) {
-             numeralArray.push("D");
-         } else if ((threeFigureArray > 5) && (threeFigureArray < 9)) {
-                numeralArray.push("D");
-                for (let i = 5; i < threeFigureArray; i++) {
-                numeralArray.push("C");
-         }
-         } else if (threeFigureArray == 9) {
-             numeralArray.push("CM");
-         }
-    
-        // Assigns numerals for values between 10 and 100.
-    
-         if (twoFigureArray <= 3) {
-            for (let i = 0; i < twoFigureArray; i++) {
-             numeralArray.push("X");
-             }
-         } else if (twoFigureArray == 4) {
-             numeralArray.push("XL");
-         } else if (twoFigureArray == 5) {
-             numeralArray.push("L");
-         } else if ((twoFigureArray > 5) && (twoFigureArray < 9)) {
-                numeralArray.push("L");
-                for (let i = 5; i < twoFigureArray; i++) {
-                numeralArray.push("X");
-         }
-         } else if (twoFigureArray == 9) {
-             numeralArray.push("XC");
-         }
-    
-        // Assigns numerals for values between 1 and 10.
-    
-         if (oneFigureArray <= 3) {
-            for (let i = 0; i < oneFigureArray; i++) {
-            numeralArray.push("I");
-            }
-        } else if (oneFigureArray == 4) {
-            numeralArray.push("IV");
-        } else if (oneFigureArray == 5) {
-            numeralArray.push("V");
-        } else if ((oneFigureArray > 5) && (oneFigureArray < 9)) {
-               numeralArray.push("V");
-               for (let i = 5; i < oneFigureArray; i++) {
-               numeralArray.push("I");
-        } 
-        } else if (oneFigureArray == 9) {
-            numeralArray.push("IX");
-        }
-    
-        // produces result value to display, taken from inside Array.
-    
-        let result = numeralArray.join("");
-    
-    
-        // displays result value on HTML pageXOffset.
-    
-        document.getElementById("numeral").innerHTML = result;
-    
+// Acquires the input value and asigns it as a variable; num.
+let num = Number(document.getElementById("decimal-input").value);
+
+// Establishes three arrays; a list of numerals, their equivalent decimal value, an empty array to receive the result.
+const numeralArray = [ 'I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M' ];
+const numberArray = [ 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 ];
+let numeralProduct = [];
+
+// Iterates down through the decimal array until num is greater than or equal to the index. The respective index is used to push a numeral to an empty array, while decreasing the value of num by that of the numberArray value at index. This process loops until num is less than the index and then continues with the parent loop.
+for (let i = numberArray.length; i >= 0; i--) {
+    while (num >= numberArray[i]) {
+        numeralProduct.push(numeralArray[i]);
+        num = num - numberArray[i];
+    }
+}
+
+// The result numerals that were pushed to numeralProduct are converted to a string and returned as a HTML output on the page.
+let result = numeralProduct.join("");
+document.getElementById("numeral").innerHTML = result;
+
 }
